89ea4cd0a992753433ac0fe5110531bd86b1b53a
Milica Bulajic <milica.bulajic@gmail.com>
2020-11-08 18:34:37 +0100

    flag finished

--- src/components/Flags/Sr.js
+++ src/components/Flags/Sr.js
@@ -3,8 +3,8 @@ import Icon from 'react-icon-base';
 
 const Sr = (props) => {
   return (
-    <Icon viewBox="0 0 640 480" {...props}>
-    <g fill-Rule="evenodd" strokeWidth="1pt">
+    <Icon viewBox="0 0 960 640" {...props}>
+    <g fillRule="evenodd" d="M60 0h1200v900H60z">
       <path fill="#fff" d="M0 0h1350v900H0z"/>
       <path fill="#0c4076" d="M0 0h1350v600H0z"/>
       <path fill="#c6363c" d="M0 0h1350v300H0z"/>
